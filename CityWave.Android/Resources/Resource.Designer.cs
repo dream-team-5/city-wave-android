f8bc283ee28970756dc7bdf29b0b7773f9acfd69
Just Me <just806me@gmail.com>
2017-11-13 11:32:03 +0200

    login page style was edited

--- CityWave.Android/Resources/Resource.Designer.cs
+++ CityWave.Android/Resources/Resource.Designer.cs
@@ -44,17 +44,17 @@ namespace CityWave.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040002
-			public const int LoginButton = 2130968578;
-			
-			// aapt resource value: 0x7f040001
-			public const int PasswordEditText = 2130968577;
-			
 			// aapt resource value: 0x7f040003
-			public const int SkipButton = 2130968579;
+			public const int LoginButton = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int PasswordEditText = 2130968578;
 			
 			// aapt resource value: 0x7f040000
-			public const int UsernameEditText = 2130968576;
+			public const int SkipButton = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int UsernameEditText = 2130968577;
 			
 			static Id()
 			{
